75ba14b54d1240b84d4f4a0ceb54bb76272f4b2e
Karthik2003cse <crkartikeyan@gmail.com>
2025-10-27 10:47:19 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// Export all middleware
-export * from './auth.js';
-export * from './validation.js';
-export * from './errorhandler.js';
-export * from './rateLimiter.js';
-export * from './logger.js';
